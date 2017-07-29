b5fdcc0c34d28c9d87b33019d49e981d0b7d09dd
Josh Martin <joshmartin5001@gmail.com>
2017-07-28 22:00:08 -0400

    .zshrc is working?

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ fi
 
 # getting started with symlinks : https://github.com/webpro/dotfiles/blob/b04b26b33df7b7331315c70e92b635f42cf01bf2/install.sh#L20
 
-#ln -sfv "$DOTFILES_DIR/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
 
