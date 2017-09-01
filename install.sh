3dcfb29c22ead1352704840b53c160bd812e0e4d
Josh Martin <chajmart@umail.iu.edu>
2017-09-01 11:13:59 -0400

    hope this works

--- install.sh
+++ install.sh
@@ -38,10 +38,4 @@ fi
 # getting started with symlinks : https://github.com/webpro/dotfiles/blob/b04b26b33df7b7331315c70e92b635f42cf01bf2/install.sh#L20
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -sfv $DOTFILES_DIR/zshrc ~/.zshrc
-ln -sfv $DOTFILES_DIR/tmux ~/.tmux.conf
-ln -sfv $DOTFILES_DIR/vimrc ~/.vimrc
-
-
-
-
+. ./linking.sh
---
+++ linking.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sfv $DOTFILES_DIR/zshrc ~/.zshrc
+ln -sfv $DOTFILES_DIR/tmux ~/.tmux.conf
+ln -sfv $DOTFILES_DIR/vimrc ~/.vimrc
+
+
