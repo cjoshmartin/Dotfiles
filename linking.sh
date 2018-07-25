799e0c32d2f38aa81c239e8af40f0aeb1eaa402b
Josh Martin <joshmartin5001@gmail.com>
2018-07-25 16:02:04 -0400

    Update linking.sh

--- linking.sh
+++ linking.sh
@@ -11,6 +11,8 @@ ln -sfv $DOTFILES_DIR/zshrc.sh ~/.zshrc
 ln -sfv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -sfv $DOTFILES_DIR/vimrc.vim ~/.vimrc
 ln -sfv $DOTFILES_DIR/alias.sh ~/.terminal_aliases
+echo " source ~/.terminal_aliases" >> .bashrc
+echo " source ~/.terminal_aliases" >> .zshrc
 
 ln -sfv $DOTFILES_DIR/ngrok ~/ngrok
 
